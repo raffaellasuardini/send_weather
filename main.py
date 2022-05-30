d5758933057a81cf78eb203a927f1d8779b28f30
raffaella <suardini.raffaella@gmail.com>
2022-05-30 12:09:02 +0200

    add sqlalchemy

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+db = SQLAlchemy(app)
+
+class Table (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+#db.create_all()
 
 @app.route("/")
 def hello_world():
