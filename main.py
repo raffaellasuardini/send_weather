03b396d43eae41ba1d221eeb7f78ffb40c9b28e8
raffaella <suardini.raffaella@gmail.com>
2022-05-30 15:04:51 +0200

    add dotenv

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+# connect to database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', "sqlite:///blog.db")
 db = SQLAlchemy(app)
 
-class Table (db.Model):
+
+class Table(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
-#db.create_all()
+
+# db.create_all()
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
