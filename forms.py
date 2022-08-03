ccf9de9096b2103fd8339776a99c6ae5c4dc50cf
raffaella <suardini.raffaella@gmail.com>
2022-08-03 18:25:13 +0200

    format forms.py

--- forms.py
+++ forms.py
@@ -6,16 +6,19 @@ from wtforms.validators import DataRequired, Email
 ##WTForm
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Inserisci la tua email', validators=[DataRequired('Inserisci una email'), Email('Email non valida')])
+    email = StringField('Inserisci la tua email',
+                        validators=[DataRequired('Inserisci una email'), Email('Email non valida')])
     location = StringField('Inserisci la città', validators=[DataRequired('Inserisci una location')])
     lat = HiddenField()
     lng = HiddenField()
     submit = SubmitField('Invia')
 
+
 class UnsusbscribeForm(FlaskForm):
     email = StringField('Inserisci la email di registrazione',
                         validators=[DataRequired('Inserisci una email'), Email('Email non valida')])
     submit = SubmitField('Invia')
+
 
 class ChangeCityForm(FlaskForm):
     email = StringField('Inserisci la email di registrazione',
